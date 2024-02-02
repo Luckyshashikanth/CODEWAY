65c81166d2f069cb5af9723585be12d7abb3c975
Luckyshashikanth <120769473+Luckyshashikanth@users.noreply.github.com>
2024-02-02 22:35:58 +0530

    Task3.java

--- Task3.java
+++ Task3.java
@@ -85,7 +85,7 @@ class ATM {
     }
 }
 
-public class Main {
+public class ATMinterface {
     public static void main(String[] args) {
         BankAccount userAccount = new BankAccount(1000.0); // Initial balance of $1000
         ATM atm = new ATM(userAccount);
